2bff9003d2987217265eca9fc30e3ef423a4c658
ramesh22788 <ramesh22788@gmail.com>
2019-05-04 13:58:08 -0700

    Code Updated with String Operatiions - Reading Strings.

--- PythonBasics/Program02Variables.py
+++ PythonBasics/Program02Variables.py
@@ -1,6 +1,6 @@
 #----Data Types In Python----
 # 1) Numbers - Int (Signed Integers)
-input_1 = 1234.09;
+input_1 = 1234;
 my_int_1=int(input_1)
 print("Welcome to Python Programming");
 print("Input Value:",input_1);
@@ -10,3 +10,13 @@ input_2 = 1234567;
 my_int_2=float(input_2)
 print("Input Value:",input_2);
 print("Output: Floating Numbers",my_int_2);
+#String Reading
+var1 = "Python Programming!!"
+var2 = "Agave Library"
+print("Python Programming - String Operations")
+print("-------------------Output1---------------------------------")
+print("Partial Read, Printing of String: ",var1);
+print ("Print only first Six Chars of Var1 - var1[0:6]:",var1[0:6])
+print("-------------------Output2---------------------------------")
+print("Partial Read, Printing of String: ",var2);
+print ("Print only last Seven Chars of Var2 - var2[6:14]:",var2[6:14])
