1e2486fa7d9f5615c7d845fdf857f1f1e8247643
ramesh22788 <ramesh22788@gmail.com>
2019-05-04 13:25:53 -0700

    Program 2 - Python Variables

---
+++ PythonBasics/Program02Variables.py
@@ -0,0 +1,12 @@
+#----Data Types In Python----
+# 1) Numbers - Int (Signed Integers)
+input_1 = 1234.09;
+my_int_1=int(input_1)
+print("Welcome to Python Programming");
+print("Input Value:",input_1);
+print("Output: Signed Integers",my_int_1);
+# 2) Numbers - float
+input_2 = 1234567;
+my_int_2=float(input_2)
+print("Input Value:",input_2);
+print("Output: Floating Numbers",my_int_2);
