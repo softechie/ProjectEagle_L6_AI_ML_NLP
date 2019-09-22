eb17e835249002830513af9773217ac59a2c93eb
softechie <softechie@gmail.com>
2019-09-22 11:14:09 -0700

    Code cleanup

--- PythonBasics/Program01StringOperations.py
+++ PythonBasics/Program01StringOperations.py
@@ -1,10 +1,10 @@
 # define punctuation
 punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 
-my_str = "Hello!!!, The Journey Starts Here... Be Patient ---moving to Agave Library ---Phoenix."
+#my_str = "Hello!!!, The Journey Starts Here... Be Patient ---moving to Agave Library ---Phoenix."
 
 # To take input from the user
-#my_str = input("Enter a string: ")
+my_str = input("Enter a string: ")
 
 # remove punctuation from the string
 no_punct = ""
